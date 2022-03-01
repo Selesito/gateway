49be86ed6883ad55f472dcf33287ea223c361be4
Vitaliy Morozov <selesitomorozov@gmail.com>
2022-03-01 16:24:03 +0300

    fix token application-dev.yml application-prod

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,16 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'shop-one',
+        data: { pageTitle: 'gatewayApp.producerShopOne.home.title' },
+        loadChildren: () => import('./producer/shop-one/shop-one.module').then(m => m.ProducerShopOneModule),
+      },
+      {
+        path: 'shop-two',
+        data: { pageTitle: 'gatewayApp.consumerShopTwo.home.title' },
+        loadChildren: () => import('./consumer/shop-two/shop-two.module').then(m => m.ConsumerShopTwoModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
